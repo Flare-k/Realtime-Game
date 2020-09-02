605d3ff5c9b060e60025310ff4b4db92c3baeb8a
Flare-k <rokkyw@naver.com>
2020-09-02 23:11:17 +0900

    [Add] Pug Template

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
+import { join } from "path";
 import express from "express";
 
 const PORT = 4000;
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views")); // 메소드명처럼 인자로 받은 경로들을 하나로 합쳐서 문자열 형태로 path를 리턴한다.
+// views(첫 인자)는 디렉토리 네임과 views 폴더(join에 들어가는 인자들)를 합친 경로를 의미한다.
+
+app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
   console.log(`✅ Server running: http://localhost:${PORT}`);
